7f4b17e7f6ab2979733e96725ec06d92a60ce3ce
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-08 12:07:25 +0800

    [Finalizing] - creating notice message at start and fixing format

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -3,66 +3,10 @@
 
 # PSEUDOCODE
 
-# Create a working simple calculator function that will serve as the basis only for creating functions inside tkinter
+# import tkinter module
 from tkinter import *
 
-# Initialize Variables
-user_loop_response = "y"
-
-while user_loop_response == "y":
-    
-    try:
-
-    #    Ask the user what operation he/she would like to use
-        print ("Select an operation. Type number only. \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division")
-
-        operation_input = int(input("Select operator: "))
-            
-        # Let the user input the first input
-        first_input = int(input("Enter first input: "))
-
-        # Let the user input the second input
-        second_input = int(input("Enter second input: "))
-
-        # Addition operation
-        if operation_input == 1:
-            sum = (first_input + second_input)
-            
-        #   Display output considering the operation that the user chose
-            print (sum)
-            
-        # Subtraction operation
-        elif operation_input == 2:
-            difference = (first_input - second_input)
-            print (difference)
-            
-        # Multiplication operation
-        elif operation_input == 3:
-            product = (first_input * second_input)
-            print (product)
-
-        else:
-            quotient = (first_input / second_input)
-            print (quotient)
-            
-        # Ask the user if he/she wants to try again
-        user_loop_response = str(input("Do you want to try again? y/n: ")).lower()
-
-    # Display error message if the user entered a string
-    except ValueError:
-        user_loop_response = "y"
-        print ("Enter an integer!")
-
-
-    # Display an error message if the user inputs a zero in 2nd input
-
-    except ZeroDivisionError:
-        user_loop_response = "y"
-        print ("The second input must not be equal to zero! Please try again.")
-        
-print ("Thank you")
-
-# Based on the created simple calculator program, create a GUI using tkinter
+# Create simple calculator program using GUI tkinter
 
 # Define a function called First_Window
 def First_Window():
